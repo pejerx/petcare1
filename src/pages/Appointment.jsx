5b8ce0809f6c338afc1fb43361df1044da821f6b
ChescaAbella <franchesca.abella0801@gmail.com>
2025-06-23 13:15:23 +0800

    changed some things

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -90,7 +90,9 @@ const Appointment = () => {
             </Grid>
 
             <Box className="appointment-footer">
-              <Button variant="outlined" color="inherit">Cancel</Button>
+              <Button variant="outlined" color="inherit" onClick={handleClose}>
+              Cancel
+            </Button>
               <Button variant="contained" color="success">Confirm</Button>
             </Box>
           </Box>
